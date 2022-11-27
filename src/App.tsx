9ab5cec91933dadde1768c8649fd83a1000d0880
jeremy-jaemin <kjm07252@gmail.com>
2022-11-27 23:24:16 +0900

    electron added

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,8 @@ import React from 'react';
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+			<h1>React Test!!!</h1>
+			<button>test button</button>
 		</div>
 	);
 }
